859cf7788446fca8f657cd57bb3a8e21db843195
trtyyrr <m17752427945@163.com>
2026-01-01 18:02:39 +0800

    更新 native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,17 +1,13 @@
 #include <jni.h>
 #include <unistd.h>
 #include <sys/prctl.h>
-#include <string>
 
-// 这里的宏需要与你内核补丁中定义的“暗号”一致
-#define PR_SET_STEALTH_ROOT 0x1337BEEF
-
+// 必须与 Kotlin 中的包名和方法名对应
 extern "C" JNIEXPORT jint JNICALL
 Java_com_stealth_manager_RootBridge_triggerKernelRoot(JNIEnv *env, jobject thiz) {
-    // 发起特殊的 prctl 系统调用
-    // 内核捕获此调用后，会直接修改当前进程的 creds
-    int result = prctl(PR_SET_STEALTH_ROOT, 1, 0, 0, 0);
+    // 0x1337BEEF 是你在 SM8650 内核补丁中设定的自定义 prctl 选项
+    prctl(0x1337BEEF, 1, 0, 0, 0); 
     
-    // 返回当前 UID，如果为 0 则表示提权成功
-    return getuid();
+    // 返回提权后的实际 UID 给 Kotlin 层
+    return getuid(); 
 }
